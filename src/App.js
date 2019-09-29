465a876aae23234aa0ecb8e332f56f21f50042b7
saurabhverma0609 <saurabh-verma@outlook.com>
2019-09-29 15:39:42 +0530

    part1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// imports
+import React, { Component } from "react";
+import Header from "./common/header/Header";
+
+class App extends Component {
+  render() {
+    return <Header />;
+  }
+}
+
+// export
+export default App;
